5b4d554b0011c3bf54f52b1488efca1e77fb9b1b
Jurek Osada <j.c.osada@gmail.com>
2020-11-20 15:39:32 +1100

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,6 +7,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Dose.destroy_all
+Cocktail.destroy_all
+
+
+
+file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+article = Cocktail.new(name: 'NES')
+article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+
 
 puts "Creating Delicious Ingredients.....mmmmmmm"
 
