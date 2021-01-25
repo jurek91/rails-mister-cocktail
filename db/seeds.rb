ee879c6bbcd45e5db25af609c55f86efc666ba71
Jurek Osada <j.c.osada@gmail.com>
2021-01-25 11:55:15 +1100

    Created banner and nav

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require "open-uri"
+require "httparty"
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
@@ -6,16 +6,16 @@ require "open-uri"
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Dose.destroy_all#   Character.create(name: 'Luke', movie: movies.first)
 Ingredient.destroy_all
-Dose.destroy_all
 Cocktail.destroy_all
 
 
 
-file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
-article = Cocktail.new(name: 'NES')
-article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+# file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+# article = Cocktail.new(name: 'NES')
+# article.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 
 
 puts "Creating Delicious Ingredients.....mmmmmmm"
